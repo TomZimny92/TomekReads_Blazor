55d12859be26d7f274199b4b9fac8ea9bedd5d66
Tom Zimny <64618507+TomZimny92@users.noreply.github.com>
2025-10-24 19:13:03 -0400

    boilerplate

---
+++ Services/BookService.cs
@@ -0,0 +1,29 @@
+﻿using TomekReads.Models;
+
+namespace TomekReads.Services
+{
+    public class BookService
+    {
+        public static async List<Book> GetAll()
+        {
+            // open the database
+            // query the database
+            // format the results
+            // return list of books
+        }
+
+        public static async Book? GetBook(int id)
+        {
+            // open the database
+            // query the database using the id
+            // format the result
+            // if no result, throw exception?
+            // otherwise, return the result
+        }
+
+        public static void AddBook(Book book)
+        {
+
+        }
+    }
+}
