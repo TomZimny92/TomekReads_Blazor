fcf5f84fb1248f2e34ed487e7cba45b50bd01113
Tom Zimny <64618507+TomZimny92@users.noreply.github.com>
2025-10-29 19:32:01 -0400

    extrapolating blazor

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TomekReads.Data;
+using TomekReads.Services;
+
+namespace TomekReads.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly BookDbContext _bookDbContext;
+        public IActionResult Home()
+        {
+            var bs = new BookService(_bookDbContext);
+            var books = bs.GetAllAsync();
+            return View(books);
+        }
+    }
+}
